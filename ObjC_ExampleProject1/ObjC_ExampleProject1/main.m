9ad1461b3294ccd7e83e3ccedc53108e2600df98
deblur99 <deblur99@kakao.com>
2023-09-12 22:39:08 +0900

    Create new repo

---
+++ ObjC_ExampleProject1/ObjC_ExampleProject1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjC_ExampleProject1
+//
+//  Created by 한현민 on 2023/09/12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
